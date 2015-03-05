03ff5bf7c2f8f11b0439b677d576c12191c46e25
Ryan Thompson <service@aiwebsystems.com>
2015-03-04 20:27:23 -0600

    MIgrations templates

---
+++ migrations/2015_03_05_021725_anomaly.module.files__100__create_files_fields.php
@@ -0,0 +1,24 @@
+<?php
+
+use Anomaly\Streams\Platform\Database\Migration\Migration;
+
+/**
+ * Class AnomalyModuleFiles_100_CreateFilesFields
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ */
+class AnomalyModuleFiles_100_CreateFilesFields extends Migration
+{
+
+    /**
+     * The module fields.
+     *
+     * @var array
+     */
+    protected $fields = [
+        
+    ];
+
+}
