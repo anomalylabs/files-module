d85fa3bb5adc51f4a58003519eb5aaa8402d6d5e
Ryan Thompson <service@aiwebsystems.com>
2015-03-14 00:41:50 -0500

    Consolidate

--- src/FilesModuleServiceProvider.php
+++ src/FilesModuleServiceProvider.php
@@ -20,10 +20,39 @@ class FilesModuleServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register('Anomaly\FilesModule\File\FileServiceProvider');
-        $this->app->register('Anomaly\FilesModule\Drive\DriveServiceProvider');
-        $this->app->register('Anomaly\FilesModule\Folder\FolderServiceProvider');
-        $this->app->register('Anomaly\FilesModule\Browser\BrowserServiceProvider');
-        $this->app->register('Anomaly\FilesModule\Adapter\StorageAdapterServiceProvider');
+        // Drive services.
+        $this->app->bind(
+            'Anomaly\FilesModule\Drive\DriveModel',
+            'Anomaly\FilesModule\Drive\DriveModel'
+        );
+
+        $this->app->bind(
+            'Anomaly\FilesModule\Drive\Contract\DriveRepositoryInterface',
+            'Anomaly\FilesModule\Drive\DriveRepository'
+        );
+
+        // File services.
+        $this->app->bind(
+            'Anomaly\FilesModule\File\FileModel',
+            'Anomaly\FilesModule\File\FileModel'
+        );
+        $this->app->bind(
+            'Anomaly\FilesModule\File\Contract\FileRepositoryInterface',
+            'Anomaly\FilesModule\File\FileRepository'
+        );
+
+        // Folder services.
+        $this->app->bind(
+            'Anomaly\FilesModule\Folder\FolderModel',
+            'Anomaly\FilesModule\Folder\FolderModel'
+        );
+
+        $this->app->bind(
+            'Anomaly\FilesModule\Folder\Contract\FolderRepositoryInterface',
+            'Anomaly\FilesModule\Folder\FolderRepository'
+        );
+
+        // Module routes.
+        $this->app->register('Anomaly\FilesModule\FilesModuleRouteProvider');
     }
 }
