41b30d98ed0a3b0e8a9a8e91a12aebed66b0cec9
Ryan Thompson <service@aiwebsystems.com>
2015-06-09 00:56:47 -0500

    K

---
+++ src/Browser/Table/BrowserTableBuilder.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\FilesModule\Browser;
+
+use Anomaly\Streams\Platform\Ui\Table\Multiple\MultipleTableBuilder;
+
+/**
+ * Class BrowserTableBuilder
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\FilesModule\Browser
+ */
+class BrowserTableBuilder extends MultipleTableBuilder
+{
+
+}
---
+++ src/Folder/Table/FolderTableBuilder.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\FilesModule\Folder\Table;
+
+use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
+
+/**
+ * Class FolderTableBuilder
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\FoldersModule\Folder\Table
+ */
+class FolderTableBuilder extends TableBuilder
+{
+
+}
