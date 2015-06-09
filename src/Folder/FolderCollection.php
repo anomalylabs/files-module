368f697a04bca6448ba082182149620c6e9180ba
Ryan Thompson <service@aiwebsystems.com>
2015-06-09 03:04:33 -0500

    Mkay

---
+++ src/Folder/FolderCollection.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\FilesModule\Folder;
+
+use Anomaly\Streams\Platform\Entry\EntryCollection;
+
+/**
+ * Class FolderCollection
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\FilesModule\Folder
+ */
+class FolderCollection extends EntryCollection
+{
+
+}
