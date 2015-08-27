337f51c6ecc8212f648814278a02bdcee23ff5f4
Marco Grueter <marco.grueter@healthworld.ch>
2015-08-27 16:23:53 +0200

    initial german translation

---
+++ resources/lang/de/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'browser'  => [
+        'title' => 'Browser',
+        'read'  => 'Kann Dateien und Ordner lesen?',
+    ],
+    'disks'    => [
+        'title'  => 'Disks',
+        'read'   => 'Kann Disks lesen?',
+        'write'  => 'Kann Disks erstellen und bearbeiten?',
+        'delete' => 'Kann Disks löschen?'
+    ],
+    'settings' => [
+        'title' => 'Einstellungen',
+        'read'  => 'Kann Einstellungen einsehen?',
+        'write' => 'Kann Einstellungen bearbeiten?'
+    ]
+];
+
