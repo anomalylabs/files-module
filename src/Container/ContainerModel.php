3055b2871f8d9a614f0c8f16750450e9c9c8c0f2
Ryan Thompson <service@aiwebsystems.com>
2015-11-10 01:56:30 -0600

    Der

--- src/Container/ContainerModel.php
+++ src/Container/ContainerModel.php
@@ -1,8 +1,6 @@
 <?php namespace Anomaly\FilesModule\Container;
 
 use Anomaly\FilesModule\Container\Contract\ContainerInterface;
-use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
-use Anomaly\Streams\Platform\Model\EloquentCollection;
 use Anomaly\Streams\Platform\Model\Files\FilesContainersEntryModel;
 
 /**
@@ -34,6 +32,7 @@ class ContainerModel extends FilesContainersEntryModel implements ContainerInter
     public function getName()
     {
         dd($this->titleName);
+
         return $this->name;
     }
 
@@ -55,76 +54,5 @@ class ContainerModel extends FilesContainersEntryModel implements ContainerInter
     public function getDescription()
     {
         return $this->description;
-    }
-
-    /**
-     * Get the title key.
-     *
-     * @return string
-     */
-    public function getTitleName()
-    {
-        // TODO: Implement getTitleName() method.
-    }
-
-    /**
-     * Get related translations.
-     *
-     * @return EloquentCollection
-     */
-    public function getTranslations()
-    {
-        // TODO: Implement getTranslations() method.
-    }
-
-    /**
-     * Get a specified relationship.
-     *
-     * @param  string $relation
-     * @return mixed
-     */
-    public function getRelation($relation)
-    {
-        // TODO: Implement getRelation() method.
-    }
-
-    /**
-     * Return whether an entry is deletable or not.
-     *
-     * @return bool
-     */
-    public function isDeletable()
-    {
-        // TODO: Implement isDeletable() method.
-    }
-
-    /**
-     * Return the object's ETag fingerprint.
-     *
-     * @return string
-     */
-    public function etag()
-    {
-        // TODO: Implement etag() method.
-    }
-
-    /**
-     * Get the entry attributes.
-     *
-     * @return array
-     */
-    public function getAttributes()
-    {
-        // TODO: Implement getAttributes() method.
-    }
-
-    /**
-     * Flush the entry model's cache.
-     *
-     * @return EntryInterface
-     */
-    public function flushCache()
-    {
-        // TODO: Implement flushCache() method.
     }
 }
