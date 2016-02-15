b3f1fd423a4e42a77afc5a0481546e593450ffaa
Ryan Thompson <ryan@pyrocms.com>
2016-02-15 13:36:26 -0600

    Oop.. I guess you do

--- src/File/FilePresenter.php
+++ src/File/FilePresenter.php
@@ -3,6 +3,7 @@
 use Anomaly\FilesModule\File\Command\GetType;
 use Anomaly\FilesModule\File\Contract\FileInterface;
 use Anomaly\Streams\Platform\Entry\EntryPresenter;
+use Anomaly\Streams\Platform\Support\Decorator;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
@@ -181,7 +182,7 @@ class FilePresenter extends EntryPresenter
         $entry = $this->object->getEntry();
 
         if ($entry && $entry->hasField($key)) {
-            return $entry->{$key};
+            return (New Decorator())->decorate($entry)->{$key};
         }
 
         return parent::__get($key);
