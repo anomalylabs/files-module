3c5d796a287eaf9b572073b1991df9b43bac8355
Ryan Thompson <ryan@pyrocms.com>
2018-07-17 07:38:11 -0500

    Fix issue with max size being G

--- src/File/Command/GetSystemMaxUploadSize.php
+++ src/File/Command/GetSystemMaxUploadSize.php
@@ -18,9 +18,34 @@ class GetSystemMaxUploadSize
      */
     public function handle()
     {
-        $post = str_replace('M', '', ini_get('post_max_size'));
-        $file = str_replace('M', '', ini_get('upload_max_filesize'));
+        $post = $this->getSize('post_max_size');
+        $file = $this->getSize('upload_max_filesize');
 
         return $file > $post ? $post : $file;
+    }
+
+    /**
+     * Get php.ini value and convert it to Mb if needed.
+     *
+     * @param $key
+     * @return float|int
+     */
+    protected function getSize($key)
+    {
+        preg_match('/([0-9]*)(K|M|G)?/im', ini_get($key), $matches);
+
+        if ($matches[2] === 'G') {
+            return $matches[1] * 1024;
+        }
+
+        if ($matches[2] === 'M') {
+            return $matches[1];
+        }
+
+        if ($matches[2] === 'K') {
+            return $matches[1] / 1024;
+        }
+
+        return $matches[1] / (1024 * 1024);
     }
 }
